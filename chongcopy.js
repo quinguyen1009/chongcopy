7c2b5b1fb36e8fd28595279d0c20826e281a5816
quinguyen1009 <64306300+quinguyen1009@users.noreply.github.com>
2020-04-30 17:23:36 +0700

    Update chongcopy.js

--- chongcopy.js
+++ chongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>XIN DUNG COPY CUA EM : <a href='http://tdbatdongsan.com.vn/du-an-aqua-city-dong-nai/'>https://batdongsanthuongluu.blogspot.com/</a>";
+    var pagelink = "<br/><br/>XIN DUNG COPY CUA EM : <a href='http://tdbatdongsan.com.vn/du-an-aqua-city-dong-nai/'>http://tdbatdongsan.com.vn/du-an-aqua-city-dong-nai/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
